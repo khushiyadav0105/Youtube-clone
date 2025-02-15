916a2c7614be234b68260f1b6643db5f69039e9c
khushiyadav0105 <khushiyadav2913@gmail.com>
2025-02-15 21:43:07 +0530

    updated code

--- src/Pages/Video/Video.jsx
+++ src/Pages/Video/Video.jsx
@@ -1,11 +1,13 @@
 
 import './Video.css'
 import PlayVideo from '../../Components/PlayVideo/PlayVideo'
+import Recommended from '../../Components/Recommended/Recommended'
 
 const Video = () => {
   return (
     <div className='play-container'>
      <PlayVideo/>
+     <Recommended/>
     </div>
   )
 }
---
+++ src/data.js
@@ -0,0 +1,2 @@
+export const API_KEY='AIzaSyAcoJB_zO49MnZZItbFhYt_1oQFQ92I76c'
+ 
